8dd3cdc96fbf3ca22b5b7548422b06dc23f01707
Abeniyo <avelox6867@gmail.com>
2025-05-31 23:32:55 +0300

    This is messaging_app project!

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def add_message(self, request, pk=None):
+        conversation = self.get_object()
+        content = request.data.get('content')
+        if content:
+            message = Message.objects.create(
+                conversation=conversation,
+                sender=request.user,
+                content=content
+            )
+            return Response(MessageSerializer(message).data)
+        return Response({"error": "Content is required"}, status=400)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
