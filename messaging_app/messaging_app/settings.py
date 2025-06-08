e9704e823863af608b42c787908262105443758d
Abeniyo <avelox6867@gmail.com>
2025-06-08 12:56:46 +0300

    this is updated messaging_app$

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -48,8 +48,9 @@ AUTH_USER_MODEL = 'chats.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
 	'rest_framework_simplejwt.authentication.JWTAuthentication',
+    'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
