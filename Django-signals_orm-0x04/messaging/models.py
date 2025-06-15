fba636df22c89ef1222482056060dcd0bb438ffd
Abeniyo <avelox6867@gmail.com>
2025-06-15 12:29:37 +0300

    Updated version

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .managers import UnreadMessagesManager 
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
@@ -11,6 +12,9 @@ class Message(models.Model):
     edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='edited_messages')
     parent_message = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
     read = models.BooleanField(default=False)
+
+    objects = models.Manager()
+    unread = UnreadMessagesManager() 
 
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
