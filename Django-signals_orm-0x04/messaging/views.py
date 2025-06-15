cf6f8b361acafd99a827fb211289772097632754
Abeniyo <avelox6867@gmail.com>
2025-06-15 12:07:26 +0300

    Updated version

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib import messages
 
-# Create your views here.
+@login_required
+def delete_user(request):
+    if request.method == "POST":
+        user = request.user
+        logout(request)
+        user.delete()
+        messages.success(request, "Your account has been deleted.")
+        return redirect('home')
+    return render(request, 'delete_account.html')
