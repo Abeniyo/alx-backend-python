ae4fe4e9a87761dacadcd388b0d75db10e12f0c5
Abeniyo <avelox6867@gmail.com>
2025-06-15 12:01:35 +0300

    Updated version

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -7,6 +7,8 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    edited_at = models.DateTimeField(null=True, blank=True)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='edited_messages')
     parent_message = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
     read = models.BooleanField(default=False)
 
